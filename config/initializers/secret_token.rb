1d4959db95e8117c6bf1f16b78ee053bb8d65d1d
dbc gogo <dbc@dbc.com>
2013-04-27 12:45:28 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FsosFe::Application.config.secret_token = '3c7aa87d48cb76fb6086b84d11d790b39474c42fc920dc4e9e723308b72c4d3606700a01be30e76c725dac1956499373364dd6e538860c5d5cac8e41d294272c'
